1902843407bb1e5dc0fc378082de9d37e728aeb8
Sajeewa-Laptop <sajeewadilshan30@gmail.com>
2025-12-02 13:50:04 +0530

    Fix S3 client builder type

--- src/main/java/com/backend/gamelibrarybackend/config/S3Config.java
+++ src/main/java/com/backend/gamelibrarybackend/config/S3Config.java
@@ -8,6 +8,7 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.s3.S3Configuration;
 
 import java.net.URI;
@@ -29,7 +30,7 @@ public class S3Config {
 
     @Bean
     public S3Client s3Client() {
-        S3Client.Builder builder = S3Client.builder()
+        S3ClientBuilder builder = S3Client.builder()
                 .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
                 .region(Region.of(region))
                 .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build());
